93f5fdf88f3d6d0bf0f9abc862ad4e9a93dc5f5e
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-09-11 22:00:59 -0400

    Update StoreData.php

--- src/esh123unicorn/factioncore/event/StoreData.php
+++ src/esh123unicorn/factioncore/event/StoreData.php
@@ -37,13 +37,10 @@ use function ucfirst; //first letter of string capital
 use esh123unicorn\factioncore\Main;
 
 class DataBackUps implements Listener{
-	
-    public $title = "§aYou have Leveled up in wizardry";
-    public $message = "§3you are now level ";
 
     private $plugin;
 	
-    public $permission;
+    public $config;
 
     public function __construct(Main $plugin) {
         $this->plugin = $plugin;
@@ -52,3 +49,153 @@ class DataBackUps implements Listener{
     public function getPlugin(){
 	return $this->plugin;
     }
+	
+    public function storeKitsUI(): void { 
+              if(!file_exists($this->plugin->kitFolder() . "/kits.yml")) {
+		 $kit = new Config($this->plugin->kitFolder() . "/kits.yml", Config::YAML);
+            	 $userinterface = [
+      	    		$kit->setNested("title", "Kits"),
+      	    		$kit->setNested("content", "pick a kit:"),
+      	    		$kit->setNested("button1", "kit1"),
+      	    		$kit->setNested("button2", "kit2"),
+      	    		$kit->setNested("button3", "kit3"),
+      	    		$kit->setNested("button4", "kit4"),
+      	    		$kit->setNested("button5", "kit5"),
+      	    		$kit->setNested("exit-button", "Exit")
+      	    	 ];
+      	    	 $kit->save();
+	      }
+    }
+	
+    public function storeKitsItems(): void {  
+              if(!file_exists($this->plugin->kitFolder() . "/config.yml")) {
+		 $config = new Config($this->plugin->kitFolder() . "/config.yml", Config::YAML);
+            	 $c = [
+      	    		$c->setNested("unlocked", "UNLOCKED"),
+      	    		$c->setNested("locked", "LOCKED"),
+      	    		$c->setNested("time", "TIME: ")
+		 ];
+	      }
+
+              if(!file_exists($this->plugin->kitFolder() . "/kits1.yml")) {
+		 $kit = new Config($this->plugin->kitFolder() . "/kit1.yml", Config::YAML);
+            	 $userinterface = [
+      	    		$kit->setNested("cooldown", 60),
+      	    		$kit->setNested("perm", "kit1.use"), 
+      	    		$kit->setNested("helmet", 310,0),
+      	    		$kit->setNested("chesplate", 311,0),
+      	    		$kit->setNested("leggings", 312,0),
+      	    		$kit->setNested("boots", 313,0),
+      	    		$kit->setNested("sword", 376,0),
+      	    		$kit->setNested("pickaxe", 278,0),
+      	    		$kit->setNested("hoe", 293,0),
+      	    		$kit->setNested("Armorenchant1", 1),
+      	    		$kit->setNested("Armorlevel1", 1),
+      	    		$kit->setNested("Armorenchant2", 2),
+      	    		$kit->setNested("Armorlevel2", 1),
+      	    		$kit->setNested("Swordenchant1", 1),
+      	    		$kit->setNested("Swordlevel1", 1),
+      	    		$kit->setNested("Swordenchant2", 2),
+      	    		$kit->setNested("Swordlevel2", 1)
+      	    	 ];
+      	    	 $kit->save();
+	      }
+	    
+              if(!file_exists($this->plugin->kitFolder() . "/kits2.yml")) {
+		 $kit = new Config($this->plugin->kitFolder() . "/kit2.yml", Config::YAML);
+            	 $userinterface = [
+      	    		$kit->setNested("cooldown", 60),
+      	    		$kit->setNested("perm", "kit1.use"), 
+      	    		$kit->setNested("helmet", 310,0),
+      	    		$kit->setNested("chesplate", 311,0),
+      	    		$kit->setNested("leggings", 312,0),
+      	    		$kit->setNested("boots", 313,0),
+      	    		$kit->setNested("sword", 376,0),
+      	    		$kit->setNested("pickaxe", 278,0),
+      	    		$kit->setNested("hoe", 293,0),
+      	    		$kit->setNested("Armorenchant1", 1),
+      	    		$kit->setNested("Armorlevel1", 1),
+      	    		$kit->setNested("Armorenchant2", 2),
+      	    		$kit->setNested("Armorlevel2", 1),
+      	    		$kit->setNested("Swordenchant1", 1),
+      	    		$kit->setNested("Swordlevel1", 1),
+      	    		$kit->setNested("Swordenchant2", 2),
+      	    		$kit->setNested("Swordlevel2", 1)
+      	    	 ];
+      	    	 $kit->save();
+	      }
+	    
+              if(!file_exists($this->plugin->kitFolder() . "/kits3.yml")) {
+		 $kit = new Config($this->plugin->kitFolder() . "/kit3.yml", Config::YAML);
+            	 $userinterface = [
+      	    		$kit->setNested("cooldown", 60),
+      	    		$kit->setNested("perm", "kit1.use"), 
+      	    		$kit->setNested("helmet", 310,0),
+      	    		$kit->setNested("chesplate", 311,0),
+      	    		$kit->setNested("leggings", 312,0),
+      	    		$kit->setNested("boots", 313,0),
+      	    		$kit->setNested("sword", 376,0),
+      	    		$kit->setNested("pickaxe", 278,0),
+      	    		$kit->setNested("hoe", 293,0),
+      	    		$kit->setNested("Armorenchant1", 1),
+      	    		$kit->setNested("Armorlevel1", 1),
+      	    		$kit->setNested("Armorenchant2", 2),
+      	    		$kit->setNested("Armorlevel2", 1),
+      	    		$kit->setNested("Swordenchant1", 1),
+      	    		$kit->setNested("Swordlevel1", 1),
+      	    		$kit->setNested("Swordenchant2", 2),
+      	    		$kit->setNested("Swordlevel2", 1)
+      	    	 ];
+      	    	 $kit->save();
+	      }
+	    
+              if(!file_exists($this->plugin->kitFolder() . "/kits4.yml")) {
+		 $kit = new Config($this->plugin->kitFolder() . "/kit4.yml", Config::YAML);
+            	 $userinterface = [
+      	    		$kit->setNested("cooldown", 60),
+      	    		$kit->setNested("perm", "kit1.use"), 
+      	    		$kit->setNested("helmet", 310,0),
+      	    		$kit->setNested("chesplate", 311,0),
+      	    		$kit->setNested("leggings", 312,0),
+      	    		$kit->setNested("boots", 313,0),
+      	    		$kit->setNested("sword", 376,0),
+      	    		$kit->setNested("pickaxe", 278,0),
+      	    		$kit->setNested("hoe", 293,0),
+      	    		$kit->setNested("Armorenchant1", 1),
+      	    		$kit->setNested("Armorlevel1", 1),
+      	    		$kit->setNested("Armorenchant2", 2),
+      	    		$kit->setNested("Armorlevel2", 1),
+      	    		$kit->setNested("Swordenchant1", 1),
+      	    		$kit->setNested("Swordlevel1", 1),
+      	    		$kit->setNested("Swordenchant2", 2),
+      	    		$kit->setNested("Swordlevel2", 1)
+      	    	 ];
+      	    	 $kit->save();
+	      }
+	    
+              if(!file_exists($this->plugin->kitFolder() . "/kits5.yml")) {
+		 $kit = new Config($this->plugin->kitFolder() . "/kit5.yml", Config::YAML);
+            	 $userinterface = [
+      	    		$kit->setNested("cooldown", 60),
+      	    		$kit->setNested("perm", "kit1.use"), 
+      	    		$kit->setNested("helmet", 310,0),
+      	    		$kit->setNested("chesplate", 311,0),
+      	    		$kit->setNested("leggings", 312,0),
+      	    		$kit->setNested("boots", 313,0),
+      	    		$kit->setNested("sword", 376,0),
+      	    		$kit->setNested("pickaxe", 278,0),
+      	    		$kit->setNested("hoe", 293,0),
+      	    		$kit->setNested("Armorenchant1", 1),
+      	    		$kit->setNested("Armorlevel1", 1),
+      	    		$kit->setNested("Armorenchant2", 2),
+      	    		$kit->setNested("Armorlevel2", 1),
+      	    		$kit->setNested("Swordenchant1", 1),
+      	    		$kit->setNested("Swordlevel1", 1),
+      	    		$kit->setNested("Swordenchant2", 2),
+      	    		$kit->setNested("Swordlevel2", 1)
+      	    	 ];
+      	    	 $kit->save();
+	      }
+    }
+}
+	    
