b9d085043fe6142082518dc49c9c668309a1bdc7
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-09-12 17:06:50 -0400

    Create Shop.php

---
+++ src/esh123unicorn/factioncore/command/Shop.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace esh123unicorn\factioncore\command;
+
+use esh123unicorn\factioncore\Main;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+use pocketmine\utils\config;
+use pocketmine\level\Level;
+use pocketmine\level\LevelExpection;
+use pocketmine\level\LevelProvider;
+use pocketmine\level\ProviderManager;
+use pocketmine\level\Position;
+
+//form ui
+use jojoe77777\FormAPI;
+use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\CustomForm;
+
+use onebone\economyapi\EconomyAPI;
+
+class Shop extends PluginCommand{
+
+    private $owner;
+    
+    private $config;
+    
+    public function __construct(string $name, Main $owner)
+    {
+        parent::__construct($name, $owner);
+        $this->owner = $owner;
+        $this->setPermission("shop.use");
+    }
+    
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+            if($sender->hasPermission("shop.use")) {
+               $this->openShop($sender);   
+            } else {
+               $sender->sendMessage("§7(§c!§7) §cYou do not have permission to use this command");
+            }
+    }
+    
+    public function openShop(Player $player) { 
+    
