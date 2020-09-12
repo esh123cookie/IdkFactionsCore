fc9ab42f08eb1f52df3972f76a08435f761e48d1
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-09-12 00:52:05 -0400

    Create EventListener.php

---
+++ src/esh123unicorn/factioncore/event/EventListener.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace esh123unicorn\factioncore\event;
+
+use esh123unicorn\factioncore\Main;
+
+//pocketmine event
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\entity\ProjectileHitEntityEvent;
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\player\PlayerExperienceChangeEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerDataSaveEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\inventory\CraftItemEvent;
+use pocketmine\event\entity\EntityMotionEvent;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\player\PlayerChatEvent;
+
+//config
+use pocketmine\utils\config;
+
+//tasks
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\scheduler\Task;
+use pocketmine\scheduler\TaskScheduler;
+
+//blocks and items
+use pocketmine\block\BlockFactory;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\Item;
+use pocketmine\item\Armor;
+use pocketmine\item\Tool;
+use pocketmine\item\ItemFactory;
+
+//effect
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+
+//pocketmine
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\math\Vector3;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\inventory\ShapedRecipe;
+use pocketmine\utils\Utils;
+
+//form ui
+use jojoe77777\FormAPI;
+use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\CustomForm;
+
+//level
+use pocketmine\level\Level;
+use pocketmine\level\LevelExpection;
+use pocketmine\level\LevelProvider;
+use pocketmine\level\ProviderManager;
+use pocketmine\level\Position;
+use pocketmine\event\Listener;
+
+//function 
+use function time;
+use function count;
+use function floor;
+use function microtime;
+use function number_format;
+use function round;
+use function strtotime; //string to time
+use function strtolower; //string lowers all letters
+use function strtoupper; //string upper letters all
+use function ucfirst; //first letter of string capital
+
+class DataBackUps implements Listener{
+
+  
