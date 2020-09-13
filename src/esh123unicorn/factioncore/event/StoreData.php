c30c43fb51ac13af4f1dc6f46bd1b4b780902a6d
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-09-13 01:22:08 -0400

    Update StoreData.php

--- src/esh123unicorn/factioncore/event/StoreData.php
+++ src/esh123unicorn/factioncore/event/StoreData.php
@@ -37,6 +37,9 @@ use function ucfirst; //first letter of string capital
 use esh123unicorn\factioncore\Main;
 
 class StoreData implements Listener{
+	
+    /** @var null  */
+    private static $instance = null;
 
     private $plugin;
 	
@@ -422,5 +425,12 @@ class StoreData implements Listener{
       	    	 ];
       	    	 $config->save(); 
 	      }
+    }
+	
+    /**
+    * @return static
+    */
+    public static function getInstance() : self {
+        return self::$instance;
     }
 }
